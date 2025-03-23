b6e589b37f6cc16505761afef89c52046875174b
lpk2berkah <lpk2berkah@gmail.com>
2025-03-23 11:29:37 +0800

    Add files via upload
    copy dari pelatihan jahit cuma beda nomor WA

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init({
+  offset: 120, // Jarak sebelum elemen mulai dianimasikan (default: 120px)
+  delay: 200, // Penundaan sebelum animasi dimulai (default: 0ms)
+  duration: 1000, // Durasi animasi dalam milidetik (default: 400ms)
+  easing: "ease-in-out", // Jenis transisi (default: 'ease')
+  once: true, // Animasi hanya terjadi sekali (true) atau setiap scroll (false)
+  mirror: false, // Apakah animasi terjadi saat menggulir ke atas kembali
+});
